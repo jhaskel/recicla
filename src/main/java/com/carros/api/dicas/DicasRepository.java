81844249a8dfa490fb7b083e7f2eb5703e845a4a
Joao Haskel <johaskel@gmail.com>
2021-06-29 16:53:26 -0300

    git teste

--- src/main/java/com/carros/api/dicas/DicasRepository.java
+++ src/main/java/com/carros/api/dicas/DicasRepository.java
@@ -10,8 +10,8 @@ interface DicasRepository extends JpaRepository<Dica, Long> {
     @Query(value = "select * from dica  order by id desc", nativeQuery = true)
     List<Dica> findAll2();
 
-    @Query(value = "SELECT COUNT(dica.id) AS quant FROM dica \n" +
-            "WHERE  dica.id NOT IN (SELECT idevento FROM favoritos WHERE icone = 3 ) ", nativeQuery = true)
+    @Query(value = "SELECT COUNT(id) AS quant FROM dica \n" +
+            "WHERE id NOT IN (SELECT idevento FROM favoritos WHERE icone = 3 AND usuario = 2) ) ", nativeQuery = true)
     long findQuantLidas();
 
 }
